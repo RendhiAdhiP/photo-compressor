00e97245a17e7d18ef4d9753e7c93650b962e8d8
RendhiAdhiP <rendhiap123@gmail.com>
2024-08-26 12:06:18 +0700

    feat:update

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import imageCompression from 'browser-image-compression';
 
 const App: React.FC = () => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
-  // const [compressedFile, setCompressedFile] = useState<File | null>(null);
   const [imagePreview, setImagePreview] = useState<string | null>(null);
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -18,27 +17,38 @@ const App: React.FC = () => {
   const handleCompressImage = async () => {
     if (!selectedFile) return;
 
-    const options = {
-      maxSizeMB: 0.299,
-      maxWidthOrHeight: 1024,
-      useWebWorker: true,
+    const img = new Image();
+    img.src = URL.createObjectURL(selectedFile);
+
+    img.onload = async () => {
+      let maxWidthOrHeight = 1200;
+
+      if (img.naturalWidth > img.naturalHeight) {
+        maxWidthOrHeight = img.naturalWidth > 1200 ? 1200 : img.naturalWidth;
+      } else {
+        maxWidthOrHeight = img.naturalHeight > 1200 ? 1200 : img.naturalHeight;
+      }
+
+      const options = {
+        maxSizeMB: 0.299,
+        maxWidthOrHeight: maxWidthOrHeight,
+        useWebWorker: true,
+        initialQuality: 1,
+      };
+
+      try {
+        const compressedImage = await imageCompression(selectedFile, options);
+        const downloadLink = URL.createObjectURL(compressedImage);
+        const link = document.createElement('a');
+        link.href = downloadLink;
+        link.download = compressedImage.name;
+        document.body.appendChild(link);
+        link.click();
+        link.remove();
+      } catch (error) {
+        console.error('Error compressing image:', error);
+      }
     };
-
-    try {
-      const compressedImage = await imageCompression(selectedFile, options);
-      // setCompressedFile(compressedImage);
-
-      // Untuk mengunduh gambar yang telah dikompres
-      const downloadLink = URL.createObjectURL(compressedImage);
-      const link = document.createElement('a');
-      link.href = downloadLink;
-      link.download = compressedImage.name;
-      document.body.appendChild(link);
-      link.click();
-      link.remove();
-    } catch (error) {
-      console.error('Error compressing image:', error);
-    }
   };
 
   return (
@@ -53,12 +63,6 @@ const App: React.FC = () => {
           <div className="flex flex-col gap-4">
             <input className="text-center p-4 border border-grey-300 rounded-xl" type="file" onChange={handleFileChange} accept="image/*" />
             <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2  px-2 rounded' onClick={handleCompressImage}>Compress Image</button>
-            {/* {compressedFile && (
-              <div>
-                <p>Original Size: {selectedFile.size / 1024} KB</p>
-                <p>Compressed Size: {compressedFile.size / 1024} KB</p>
-              </div>
-            )} */}
           </div>
         </div>
       </div>
